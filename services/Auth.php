661e5cd556f5cff22c4459ad334b240b424a90ee
Rafael-Santos2 <rafael.araujo03.07.07@gmail.com>
2025-04-29 10:21:04 -0300

    att

--- services/Auth.php
+++ services/Auth.php
@@ -2,19 +2,22 @@
 // Define espaço pra organização do código
 namespace Services;
 
-class Auth{
+class Auth
+{
     // Atributos
     private array $usuarios = [];
 
     // Método construtor para adicionar um usuário
-    public function __construct(){
-        $this ->carregarUsuarios();     
+    public function __construct()
+    {
+        $this->carregarUsuarios();
     }
 
     // Método para carregar os usuários do arquivo JSON
-    private function carregarUsuarios(): void{
+    private function carregarUsuarios(): void
+    {
         // Verifica se o arquivo existe
-        if (file_exists('ARQUIVO_USUARIOS')){
+        if (file_exists('ARQUIVO_USUARIOS')) {
             // Lê o conteúdo e decodifica o JSON para o array
             $conteudo = json_decode(file_get_contents('ARQUIVO_USUARIOS'), true);
             // Verifica se o conteúdo não é nulo e é um array
@@ -33,22 +36,24 @@ class Auth{
                     'perfil' => 'usuario'
                 ]
             ];
-            $this ->salvarUsuarios(); // Salva os usuários no arquivo
+            $this->salvarUsuarios(); // Salva os usuários no arquivo
         }
     }
     // Função para salvar os usuários no arquivo JSON
-    private function salvarUsuarios(): void{
-        $dir = dirname(ARQUIVO_USUARIOS);
+    private function salvarUsuarios(): void
+    {
+        $dir = dirname(ARQUIVO_USUARIO);
 
-        if (!is_dir($dir)){
+        if (!is_dir($dir)) {
             mkdir($dir, 0777, true); // Cria o diretório se não existir
         }
         // Salva os usuários no arquivo JSON
-        file_put_contents(ARQUIVO_USUARIOS, json_encode($this->usuarios, JSON_PRETTY_PRINT));
+        file_put_contents(ARQUIVO_USUARIO, json_encode($this->usuarios, JSON_PRETTY_PRINT));
     }
 
     // Método para login de usuario
-    public function login(string $username, string $password): bool{
+    public function login(string $username, string $password): bool
+    {
         // Verifica se o usuário existe no array
         foreach ($this->usuarios as $usuario) {
             if ($usuario['username'] === $username && password_verify($password, $usuario['password'])) {
@@ -63,24 +68,29 @@ class Auth{
         return false; // Retorna falso se o login falhar
     }
 
-    public function logout(): void{
+    public function logout(): void
+    {
         // Remove a sessão de autenticação
         session_destroy();
     }
-    
+
     // Método para verificar se o usuário está logado
-    public static function verificarLogin(): bool{
+    public static function verificarLogin(): bool
+    {
         return isset($_SESSION['auth']) && $_SESSION['auth']['perfil'] === true;
     }
 
-    public static function isPerfil(string $perfil): bool{
+    public static function isPerfil(string $perfil): bool
+    {
         return isset($_SESSION['auth']) && $_SESSION['auth']['perfil'] === $perfil;
     }
 
-    public static function isAdmin(): bool{
+    public static function isAdmin(): bool
+    {
         return self::isPerfil('admin');
     }
-    public static function getUsuario(): ?array{
+    public static function getUsuario(): ?array
+    {
         return $_SESSION['auth'] ?? null;
     }
 }
